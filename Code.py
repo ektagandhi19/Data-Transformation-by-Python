5186886561d736c529a799b7671cb4192500de0c
Ekta Gandhi <96858675+ektagandhi19@users.noreply.github.com>
2022-07-08 11:59:14 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -5,19 +5,19 @@
 
 
 import pandas as pd
-data = pd.read_excel("C:\\Users\\Ekta Gandhi\\Downloads\\sample_input.xlsx",index_col=0)
+data = pd.read_excel("D:\\sample_input.xlsx",index_col=0)
 df = pd.DataFrame(data)
-balance = [df.loc[df['Name'] == 'Andrew', 'Balance'].sum(), df.loc[df['Name'] == 'Kylie', 'Balance'].sum(), df.loc[df['Name'] == 'James', 'Balance'].sum()]
-credit = [df.loc[df['Name'] == 'Andrew', 'Credit'].sum(), df.loc[df['Name'] == 'Kylie', 'Credit'].sum(), df.loc[df['Name'] == 'James', 'Credit'].sum()]
-Name = pd.DataFrame(set(df['Name']))
+Name = pd.DataFrame(list(df['Name'].drop_duplicates()))
 Andrew_age = df.loc[df['Name'] == 'Andrew', 'Age']
-Kylie_age = df.loc[df['Name'] == 'Kylie', 'Age']
 James_age = df.loc[df['Name'] == 'James', 'Age']
-Age = [list(Andrew_age), list(Kylie_age), list(James_age)]
+Kylie_age = df.loc[df['Name'] == 'Kylie', 'Age']
+Age = [list(Andrew_age), list(James_age), list(Kylie_age)]
+balance = [df.loc[df['Name'] == 'Andrew', 'Balance'].sum(), df.loc[df['Name'] == 'James', 'Balance'].sum(), df.loc[df['Name'] == 'Kylie', 'Balance'].sum()]
+credit = [df.loc[df['Name'] == 'Andrew', 'Credit'].sum(), df.loc[df['Name'] == 'James', 'Credit'].sum(), df.loc[df['Name'] == 'Kylie', 'Credit'].sum()]
 Net_bal = pd.DataFrame(balance)-pd.DataFrame(credit)
 df1=pd.DataFrame()
 df1['Name']=Name
 df1['Age']=Age
 df1['Net (Balance-Credit)']=Net_bal
-df1.to_excel('D:\Output.xlsx')
+df1.to_excel('D:\Output1.xlsx')
 
